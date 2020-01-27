c3dd28fa13d976da8940d5148af69bf2c7b0cefb
karleee <karleee@protonmail.com>
2020-01-27 15:34:38 -0800

    Rendering root component

--- spotify/frontend/index.jsx
+++ spotify/frontend/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -7,8 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
 
   //testing
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+//   window.getState = store.getState;
+//   window.dispatch = store.dispatch;
 
-  ReactDOM.render(<h1>Success!</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
