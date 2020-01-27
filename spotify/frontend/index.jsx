faf28d2546114509f9d824c4f4822a72c4a78a10
karleee <karleee@protonmail.com>
2020-01-27 15:26:35 -0800

    Finished configureStore

--- spotify/frontend/index.jsx
+++ spotify/frontend/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+
+  //testing
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
   ReactDOM.render(<h1>Success!</h1>, root);
 });
