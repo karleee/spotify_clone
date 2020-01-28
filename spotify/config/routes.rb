8c8bf0d918eb5fba8906eca35339193cc9e08af2
karleee <karleee@protonmail.com>
2020-01-28 15:37:19 -0800

    File cleaning

--- spotify/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  namespace :api, defaults: {format: :json} do
-    resources :users, only: :create
-    resources :session, only: [:create, :destroy]
-  end
-
-  root 'static_pages#root'
-end
