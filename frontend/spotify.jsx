ca61999ccab5268226a94ad6d45d05ff1696e28b
karleee <karleee@protonmail.com>
2020-01-28 16:32:43 -0800

    Finished session api util

--- frontend/spotify.jsx
+++ frontend/spotify.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as APIUtil from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
+  window.login = APIUtil.login;
+  window.signup = APIUtil.signup;
+  window.logout = APIUtil.logout;
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Success</h1>, root)
 })
