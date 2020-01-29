65eb232511b4e01facf546b350b1ab7480855b1c
karleee <karleee@protonmail.com>
2020-01-28 16:24:55 -0800

    Finished auth backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+
+  helper_method :current_user, :loggedin?
+
+  private
+  
+  def log_in(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user = User.find_by(session_token: session[:session_token])
+  end
+
+  def log_out
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def loggedin?
+    !!current_user
+  end
 end
