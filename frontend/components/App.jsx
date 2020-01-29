11137781fc25fea28f3782b1ba359bc06677cc9c
karleee <karleee@protonmail.com>
2020-01-28 16:53:55 -0800

    Finished react router and session components

---
+++ frontend/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+const App = () => (
+  <div>
+    <h1>Spotify</h1>
+  </div>
+);
+
+export default App;
--- frontend/spotify.jsx
+++ frontend/spotify.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import * as APIUtil from './util/session_api_util';
 import configureStore from './store/store';
+import Root from './components/root';
+
+// import * as APIUtil from './util/session_api_util';
+
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.login = APIUtil.login;
-  window.signup = APIUtil.signup;
-  window.logout = APIUtil.logout;
+//   window.login = APIUtil.login;
+//   window.signup = APIUtil.signup;
+//   window.logout = APIUtil.logout;
 
   const root = document.getElementById('root');
   const store = configureStore();
 
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  
-  ReactDOM.render(<h1>Success</h1>, root)
+
+  ReactDOM.render(<Root store={store} />, root)
 })
