481519e19ea74544bba5a2dcad5cd8a118757423
karleee <karleee@protonmail.com>
2020-01-28 16:49:36 -0800

    Finished store

--- frontend/spotify.jsx
+++ frontend/spotify.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import * as APIUtil from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   window.login = APIUtil.login;
@@ -9,5 +10,10 @@ document.addEventListener('DOMContentLoaded', () => {
   window.logout = APIUtil.logout;
 
   const root = document.getElementById('root');
+  const store = configureStore();
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  
   ReactDOM.render(<h1>Success</h1>, root)
 })
